2d0e395d2c8f3180d1bb44d7f2ddd718b6d03b73
jerra <jerrakirstenr22@gmail.com>
2024-05-28 00:23:20 +0800

    Images Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,31 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const typingTextElement = document.querySelector('.typing-text');
+    const words = ['Web Developer', 'Backend Developer', 'Web Designer', 'Software Engineer', 'Software Developer'];
+    let wordIndex = 0;
+    let charIndex = 0;
+    let typing = true;
+
+    function type() {
+        const currentWord = words[wordIndex];
+        if (typing) {
+            typingTextElement.textContent = currentWord.substring(0, charIndex++);
+            if (charIndex > currentWord.length) {
+                typing = false;
+                setTimeout(() => typing = true, 2000);
+            }
+        } else {
+            typingTextElement.textContent = currentWord.substring(0, charIndex--);
+            if (charIndex < 0) {
+                typing = true;
+                wordIndex = (wordIndex + 1) % words.length;
+            }
+        }
+        setTimeout(type, typing ? 100 : 50);
+    }
+
+    type();
+});
